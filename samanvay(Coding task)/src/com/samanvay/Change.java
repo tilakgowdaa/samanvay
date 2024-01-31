ed072636de75357d1bc3be6bd531ed1e1205c11c
tilakgowdaa <tilakgowda.xworkz@gmail.com>
2024-01-31 16:46:00 +0530

    Task by samanvay

--- samanvay(Coding task)/src/com/samanvay/Change.java
+++ samanvay(Coding task)/src/com/samanvay/Change.java
@@ -13,17 +13,14 @@ public class Change {
 		System.out.print("Amount Paid: ");
 		double amountPaid = scanner.nextDouble();
 
-		double itemPriceInPaise = itemPrice * 100;
-		double amountPaidInPaise = (amountPaid * 100);
-
-		int remaininfAmountInPaise = (int) (amountPaidInPaise - itemPriceInPaise);
-		System.out.println(remaininfAmountInPaise);
 		scanner.close();
-		Change.getChange(remaininfAmountInPaise);
+		Change.getChange(amountPaid, itemPrice);
 	}
 
-	private static void getChange(int remaininfAmountInPaise) {
-
+	private static void getChange(double amountPaid, double itemPrice) {
+		double itemPriceInPaise = itemPrice * 100;
+		double amountPaidInPaise = amountPaid * 100;
+		int remaininfAmountInPaise = (int) (amountPaidInPaise - itemPriceInPaise);
 		int[] denominations = { 100, 50, 25, 10, 5, 1 };
 		int[] change = new int[denominations.length];
 
